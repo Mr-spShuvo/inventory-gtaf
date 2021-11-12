5e74ee2ebdc16f0912680fd743700cbd6871c5fa
Shahriar Parvez <mr.spshuvo@gmail.com>
2021-11-12 06:12:26 +0600

    :rocket: initial upload + add tailwind config

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-100 flex items-center justify-center h-screen">
+      <h1 className="text-center text-xl font-bold">Hello React App (+tailwind)</h1>
+    </div>
+  );
+}
+
+export default App;
